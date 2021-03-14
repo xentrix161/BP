692094688c0786ca08f0686016ab14f8f69d1237
xentrix161 <xentrix161@users.noreply.github.com>
2021-03-14 05:00:09 +0100

    vytvoril som order a upravil usera

---
+++ src/Form/OrderType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Order;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OrderType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date')
+            ->add('address')
+            ->add('user_id')
+            ->add('total_price')
+            ->add('invoice_number')
+            ->add('payment_method')
+            ->add('paid')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Order::class,
+        ]);
+    }
+}
