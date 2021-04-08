8b8f4c73e75b8326d3d0755556052a7ccd9e4e5f
xentrix161 <xentrix161@users.noreply.github.com>
2021-04-08 22:26:51 +0200

    ShoppingCartService predpripraveny

--- src/Services/ShoppingCartService.php
+++ src/Services/ShoppingCartService.php
@@ -4,30 +4,121 @@
 namespace App\Services;
 
 
-class ShoppingCartService
+use App\Entity\ShoppingCart;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ShoppingCartService extends AbstractController
 {
-    public function set()
+    public const STATUS_DELETED = 'CART_DELETED';
+    public const STATUS_PENDING = 'CART_PENDING';
+    public const STATUS_PAYMENT = 'CART_PAYMENT';
+    public const STATUS_DONE = 'CART_DONE';
+
+    private $isAdmin = false;
+    private $ownerId = null;
+    private $content = null;
+    private $status = null;
+    private $totalPrice = null;
+
+    public function asAdmin(): self
     {
-        //TODO: nasetuje tovar zo session items kosiku do tabulkoveho kosiku
+        $this->isAdmin = true;
+        return $this;
     }
 
-    public function updateStatus()
+    public function update(): void
     {
-       //TODO: updatne status kosiku
+        if (is_null($this->ownerId)) {
+            $this->reset();
+            return;
+        }
+
+        $cart = $this->getDoctrine()->getRepository(ShoppingCart::class)
+            ->findOneBy(['user_id' => $this->ownerId, 'status' => self::STATUS_PENDING]);
+
+        if (empty($cart)) {
+            $this->reset();
+            return;
+        }
+
+        if (!is_null($this->content)) {
+            $cart->setCartContent($this->content);
+        }
+
+        if (!is_null($this->status)) {
+            $cart->setStatus($this->status);
+        }
+
+        if (!is_null($this->totalPrice)) {
+            $cart->setTotalPrice($this->totalPrice);
+        }
+
+        if ($this->isAdmin) {
+            $cart->setUserId($this->ownerId);
+        }
+
+        //TODO: updatne existujuci zaznam
+        $this->reset();
     }
 
-    public function clear()
+    public function create(): void
     {
-        //TODO: zmaze cely riadok s kosikom v DB
+        $shoppingCart = new ShoppingCart();
+        //TODO: vytovri novy zaznam
+        $this->reset();
     }
 
-    public function updateTotalPrice()
+    public function setOwnerId(int $userId): self
     {
-        //TODO: updatne totalPRice
+        //TODO: nasetuje ID vlastnika kosiku
+        return $this;
     }
 
-    public function updateContent()
+    public function setContent(array $content): self
     {
-        //TODO: updatne tovary v kosiku
+        //TODO: nasetuje article do kosiku
+        return $this;
     }
+
+    public function setStatus(string $status): self
+    {
+        //TODO: nasetuje status kosika
+        return $this;
+    }
+
+    public function setTotalPrice(float $totalPrice): self
+    {
+        //TODO: nasetuje celkovu cenu za kosik
+        return $this;
+    }
+
+    public function getAllCarts(): array
+    {
+        //TODO: vrati vsetky kosiky podla setnuteho ID usera, ak je admin vrati aj DELETED kosiky, ak nie je tak ostatne
+        return [];
+    }
+
+    public function getContent(): array
+    {
+        //TODO: vrati obsah konkretneho kosika
+        return [];
+    }
+
+    private function reset(): void
+    {
+        $this->isAdmin = false;
+        $this->ownerId = null;
+        $this->content = null;
+        $this->status = null;
+        $this->totalPrice = null;
+    }
+
+    private function getDate(): \DateTime
+    {
+        return new \DateTime();
+    }
+
+
+
+
 }
